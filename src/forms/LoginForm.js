0c50f7001793cb2105f4dcfeda89b89f6bb912d0
Octowl <alsaff1987@gmail.com>
2019-01-10 17:58:23 +0300

    Implement signup and login

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+// Store
+import authStore from "../stores/authStore";
+
 class Login extends Component {
   state = {
     username: "",
@@ -12,7 +15,7 @@ class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    alert("I DON'T WORK YET");
+    authStore.login(this.state);
   };
 
   render() {
