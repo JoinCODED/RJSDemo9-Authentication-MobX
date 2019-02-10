0192d960a93d7e10671ffb9b1cbdfbe569e82b68
Octowl <alsaff1987@gmail.com>
2019-02-10 16:13:56 +0300

    back to fc

--- src/Logout.js
+++ src/Logout.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import { observer } from "mobx-react";
 
 import authStore from "./store/authStore";
 
-class Logout extends Component {
-  render() {
-    return (
-      <button className="btn btn-danger" onClick={() => authStore.logout()}>
-        Logout {authStore.user.username}
-      </button>
-    );
-  }
-}
+const Logout = () => (
+  <button className="btn btn-danger" onClick={() => authStore.logout()}>
+    Logout {authStore.user.username}
+  </button>
+);
 
 export default observer(Logout);
