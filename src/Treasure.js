737c9c67e519c3472d715f182c90554facef252b
Octowl <alsaff1987@gmail.com>
2019-02-10 15:44:29 +0300

    redirect logged out users

--- src/Treasure.js
+++ src/Treasure.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { Redirect } from "react-router-dom";
 
-// Store
+// Stores
 import thingStore from "./store/thingStore";
+import authStore from "./store/authStore";
+
+// Components
 import ThingItem from "./ThingItem";
 
 class Treasure extends Component {
@@ -14,6 +18,8 @@ class Treasure extends Component {
     const rows = thingStore.treasure.map(thing => (
       <ThingItem key={thing.name} thing={thing} />
     ));
+
+    if (!authStore.user) return <Redirect to="/login" />;
 
     return (
       <div className="mt-5 mx-auto col-6 text-center">
